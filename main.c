99ccdfd1cd8b3cd40820c14a10b916aea87948a4
jczornik <jczornik@graylight-imaging.com>
2023-06-16 22:18:31 +0200

    Add method for sending notifications

--- main.c
+++ main.c
@@ -34,15 +34,8 @@ conn_free:
 }
 
 bool
-send_notification ()
+read_line_from_file (const char *path, char **line, size_t *len)
 {
-  return true;
-}
-
-bool
-get_battery_capacity (int *capacity)
-{
-  const char *path = "/sys/class/power_supply/BAT0/capacity";
   bool res = true;
   FILE *file = fopen (path, "r");
 
@@ -52,31 +45,86 @@ get_battery_capacity (int *capacity)
       return false;
     }
 
-  char *line = NULL;
-  size_t len = 0;
-
-  int ret = getline (&line, &len, file);
+  int ret = getline (line, len, file);
 
   if (ret == -1)
     {
       fprintf (stderr, "Error whie reading from file: %s\n", path);
+      free (*line);
+      *line = NULL;
       res = false;
-      goto capacity_free;
+    }
+
+  fclose (file);
+  return res;
+}
+
+bool
+get_battery_capacity (int *capacity)
+{
+  const char *path = "/sys/class/power_supply/BAT0/capacity";
+  char *line = NULL;
+  size_t len = 0;
+
+  if (!read_line_from_file (path, &line, &len))
+    {
+      fprintf (stderr, "Cannot read battery capacity\n");
+      return false;
     }
 
   *capacity = atoi (line);
+  return true;
+}
 
-capacity_free:
-  free (line);
-  fclose (file);
-  return res;
+bool
+send_user_notification (DBusConnection *conn)
+{
+  DBusMessage *message = dbus_message_new_method_call (
+      "org.freedesktop.Notifications", "/org/freedesktop/Notifications",
+      "org.freedesktop.Notifications", "Notify");
+  DBusMessageIter iter[4];
+  dbus_message_iter_init_append (message, iter);
+  char *application = "sbw";
+  dbus_message_iter_append_basic (iter, 's', &application);
+  unsigned id = 0;
+  dbus_message_iter_append_basic (iter, 'u', &id);
+  char *icon = "dialog-information";
+  dbus_message_iter_append_basic (iter, 's', &icon);
+  char *summary = "Hello world!";
+  dbus_message_iter_append_basic (iter, 's', &summary);
+  char *body = "This is an example notification.";
+  dbus_message_iter_append_basic (iter, 's', &body);
+  dbus_message_iter_open_container (iter, 'a', "s", iter + 1);
+  dbus_message_iter_close_container (iter, iter + 1);
+  dbus_message_iter_open_container (iter, 'a', "{sv}", iter + 1);
+  dbus_message_iter_open_container (iter + 1, 'e', 0, iter + 2);
+  char *urgency = "urgency";
+  dbus_message_iter_append_basic (iter + 2, 's', &urgency);
+  dbus_message_iter_open_container (iter + 2, 'v', "y", iter + 3);
+  enum
+  {
+    LOW,
+    NORMAL,
+    CRITICAL
+  };
+  unsigned char level = CRITICAL;
+  dbus_message_iter_append_basic (iter + 3, 'y', &level);
+  dbus_message_iter_close_container (iter + 2, iter + 3);
+  dbus_message_iter_close_container (iter + 1, iter + 2);
+  dbus_message_iter_close_container (iter, iter + 1);
+  int timeout = 5 * 1000;
+  dbus_message_iter_append_basic (iter, 'i', &timeout);
+  dbus_connection_send (conn, message, 0);
+  dbus_connection_flush (conn);
+  dbus_message_unref (message);
+  return true;
 }
 
 int
 main ()
 {
   DBusConnection *connection = NULL;
-  if (connect_to_dbus (&connection))
+  if (!connect_to_dbus (&connection))
     {
       fprintf (stderr, "Cannot connect to dbus. Exiting...");
       exit (CANNOT_CONN_TO_DBUS_FATAL);
@@ -85,6 +133,7 @@ main ()
   int capacity = 0;
   get_battery_capacity (&capacity);
   printf ("Capacity: %d\n", capacity);
+  send_user_notification (connection);
 
   dbus_connection_unref (connection);
   return 0;
